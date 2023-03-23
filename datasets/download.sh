f286f5fddb3ae02e14cb390aeeb6081474c70986
chqwer2 <ch981129@126.com>
2023-03-23 22:18:13 +0800

    update

---
+++ datasets/download.sh
@@ -0,0 +1,40 @@
+
+
+# Download FLAME
+# https://github.com/AlirezaShamsoshoara/Fire-Detection-UAV-Aerial-Image-Classification-Segmentation-UnmannedAerialVehicle
+# On IEEE Portle:
+# FLAME 2
+# https://www.bing.com/ck/a?!&&p=c72b9636e898ca40JmltdHM9MTY3OTUyOTYwMCZpZ3VpZD0zYTU3OGI0Ny1mNWYyLTYxYTEtMDNkNi05OWQxZjRkYzYwN2YmaW5zaWQ9NTE2Ng&ptn=3&hsh=3&fclid=3a578b47-f5f2-61a1-03d6-99d1f4dc607f&u=a1aHR0cHM6Ly9pZWVlLWRhdGFwb3J0Lm9yZy9vcGVuLWFjY2Vzcy9mbGFtZS0yLWZpcmUtZGV0ZWN0aW9uLWFuZC1tb2RlbGluZy1hZXJpYWwtbXVsdGktc3BlY3RyYWwtaW1hZ2UtZGF0YXNldA&ntb=1
+
+https://ieee-dataport.org/open-access/flame-dataset-aerial-imagery-pile-burn-detection-using-drones-uavs
+
+# https://github.com/AlirezaShamsoshoara/Fire-Detection-UAV-Aerial-Image-Classification-Segmentation-UnmannedAerialVehicle
+
+
+Repository/frames/Training
+                    ├── Fire/*.jpg
+                    ├── No_Fire/*.jpg
+
+
+Repository/frames/Segmentation/Data
+                                ├── Images/*.jpg
+                                ├── Masks/*.png
+
+
+
+#UC Merced Wildfire Dataset
+
+#https://www.kaggle.com/datasets/phylake1337/fire-dataset
+
+RVL-CDIP dataset
+
+CNES Fire Detection Benchmark Dataset
+
+WildFireNet Dataset
+
+Firenet Dataset
+
+Smoke Detection UAV Dataset
+
+Aerial Fire Fighting Dataset 
+
