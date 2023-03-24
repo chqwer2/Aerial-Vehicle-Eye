a1870c8b78ec1bd7d2b146204aab534a8fd1c4f7
Calvin Chen <calvin.chen@asml.com>
2023-03-24 21:16:24 +0800

    download

--- detection/run.py
+++ detection/run.py
@@ -8,10 +8,13 @@ from ultralytics import YOLO
 
 # Load a model
 # model = YOLO("yolov8n.yaml")  # build a new model from scratch
+
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+print("load model successed...")
+
 
 # Use the model
-model.train(data="flame.yaml", epochs=3)  # train the model
+model.train(data="./detection/flame.yaml", epochs=3)  # train the model
 metrics = model.val()  # evaluate model performance on the validation set
 
 
